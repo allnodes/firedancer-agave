firedancer-io/agave

firedancer: send executed transaction hashes

ledger-tool/src/ledger_utils.rs
@@ -59,6 +59,8 @@ use {
 #[no_mangle]
 extern "C" fn fd_ext_poh_publish_leader_schedule( _data: *const u8, _len: u64 ) {}
 #[no_mangle]
+extern "C" fn fd_ext_poh_publish_executed_txn(data: *const u8) {}
+#[no_mangle]
 extern "C" fn fd_ext_larger_max_cost_per_block() -> i32 { 0 }
 #[no_mangle]
 extern "C" fn fd_ext_larger_shred_limits_per_block() -> i32 { 0 }

allnodes/firedancer-agave

ledger-tool/src/ledger_utils.rs
@@ -58,6 +58,8 @@ use {
 #[no_mangle]
 extern "C" fn fd_ext_poh_publish_leader_schedule( _data: *const u8, _len: u64 ) {}
 #[no_mangle]
+extern "C" fn fd_ext_poh_publish_executed_txn( _data: *const u8 ) {}
+#[no_mangle]
 extern "C" fn fd_ext_larger_max_cost_per_block() -> i32 { 0 }
 #[no_mangle]
 extern "C" fn fd_ext_larger_shred_limits_per_block() -> i32 { 0 }