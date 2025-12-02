e658f68603ec3effb5aedc7e8a572106c14df093
Sephiroth <sephiroth@abyss.finance>
2025-12-02 15:08:23 +0000

    Allnodes Patch

--- core/src/replay_stage.rs
+++ core/src/replay_stage.rs
@@ -97,16 +97,20 @@ pub const MAX_ENTRY_RECV_PER_ITER: usize = 512;
 pub const SUPERMINORITY_THRESHOLD: f64 = 1f64 / 3f64;
 pub const MAX_UNCONFIRMED_SLOTS: usize = 5;
 pub const DUPLICATE_LIVENESS_THRESHOLD: f64 = 0.1;
-pub const DUPLICATE_THRESHOLD: f64 = 1.0 - SWITCH_FORK_THRESHOLD - DUPLICATE_LIVENESS_THRESHOLD;
+allnodes_client::constants! {
+pub const DUPLICATE_THRESHOLD: f64 = 1.0 - *SWITCH_FORK_THRESHOLD - DUPLICATE_LIVENESS_THRESHOLD;
+}
 
 const MAX_VOTE_SIGNATURES: usize = 200;
 const MAX_VOTE_REFRESH_INTERVAL_MILLIS: usize = 5000;
 const MAX_REPAIR_RETRY_LOOP_ATTEMPTS: usize = 10;
 
-#[cfg(test)]
-static_assertions::const_assert!(REFRESH_VOTE_BLOCKHEIGHT < solana_clock::MAX_PROCESSING_AGE);
+// #[cfg(test)]
+// static_assertions::const_assert!(REFRESH_VOTE_BLOCKHEIGHT < solana_clock::MAX_PROCESSING_AGE);
+allnodes_client::constants! {
 // Give at least 4 leaders the chance to pack our vote
 const REFRESH_VOTE_BLOCKHEIGHT: usize = 16;
+}
 #[derive(PartialEq, Eq, Debug)]
 pub enum HeaviestForkFailures {
     LockedOut(u64),
@@ -573,6 +577,7 @@ impl ReplayStage {
         config: ReplayStageConfig,
         senders: ReplaySenders,
         receivers: ReplayReceivers,
+        mut voting_patch: crate::allnodes::VotingPatch,
     ) -> Result<Self, String> {
         let ReplayStageConfig {
             vote_account,
@@ -948,6 +953,11 @@ impl ReplayStage {
                             &bank_forks,
                         );
 
+                        let mostly_confirmed_slots = voting_patch.populate_mostly_confirmed_slots(
+                            &progress,
+                            &bank_forks,
+                            fork_stats,
+                        );
                         Self::mark_slots_duplicate_confirmed(
                             &duplicate_confirmed_forks,
                             &blockstore,
@@ -961,6 +971,10 @@ impl ReplayStage {
                             &mut purge_repair_slot_counter,
                             &mut tbft_structs.duplicate_confirmed_slots,
                         );
+                        for slot in mostly_confirmed_slots {
+                            let slot_progress = progress.get_mut(&slot).unwrap();
+                            slot_progress.fork_stats.is_mostly_confirmed = true;
+                        }
                     }
                     compute_slot_stats_time.stop();
 
@@ -1026,6 +1040,7 @@ impl ReplayStage {
                     }
                     heaviest_fork_failures_time.stop();
 
+                    voting_patch.update_config();
                     let mut voting_time = Measure::start("voting_time");
                     // Vote on a fork
                     if let Some((ref vote_bank, ref switch_fork_decision)) = vote_bank {
@@ -1040,31 +1055,57 @@ impl ReplayStage {
                             );
                         }
 
-                        Self::handle_votable_bank(
-                            vote_bank,
-                            switch_fork_decision,
-                            &bank_forks,
-                            &mut tower,
-                            &mut progress,
-                            &vote_account,
-                            &identity_keypair,
-                            &authorized_voter_keypairs.read().unwrap(),
-                            &blockstore,
-                            &leader_schedule_cache,
-                            &lockouts_sender,
-                            snapshot_controller.as_deref(),
-                            rpc_subscriptions.as_deref(),
-                            &block_commitment_cache,
-                            &bank_notification_sender,
-                            &mut tracked_vote_transactions,
-                            &mut has_new_vote_been_rooted,
-                            &mut replay_timing,
-                            &voting_sender,
-                            &drop_bank_sender,
-                            wait_to_vote_slot,
-                            &mut first_alpenglow_slot,
-                            &mut tbft_structs,
-                        );
+                        let (vote_banks, pop_expired) = voting_patch
+                            .populate_vote_banks(&mut tower, vote_bank, &progress, &ancestors);
+
+                        if !vote_banks.is_empty() {
+                            for bank in vote_banks.iter() {
+                                Self::handle_votable_bank(
+                                    bank,
+                                    &bank_forks,
+                                    &mut tower,
+                                    &mut progress,
+                                    &vote_account,
+                                    &identity_keypair,
+                                    &blockstore,
+                                    &leader_schedule_cache,
+                                    &lockouts_sender,
+                                    snapshot_controller.as_deref(),
+                                    rpc_subscriptions.as_deref(),
+                                    &block_commitment_cache,
+                                    &bank_notification_sender,
+                                    &mut tracked_vote_transactions,
+                                    &mut has_new_vote_been_rooted,
+                                    &mut replay_timing,
+                                    &drop_bank_sender,
+                                    &mut first_alpenglow_slot,
+                                    &mut tbft_structs,
+                                    pop_expired,
+                                );
+                            }
+
+                            info!(
+                                "voting for window: {:?}",
+                                vote_banks
+                                    .iter()
+                                    .map(|bank| bank.slot())
+                                    .collect::<Vec<_>>()
+                            );
+
+                            Self::push_vote(
+                                vote_banks.last().unwrap(),
+                                &vote_account,
+                                &identity_keypair,
+                                &authorized_voter_keypairs.read().unwrap(),
+                                &mut tower,
+                                switch_fork_decision,
+                                &mut tracked_vote_transactions,
+                                has_new_vote_been_rooted,
+                                &mut replay_timing,
+                                &voting_sender,
+                                wait_to_vote_slot,
+                            );
+                        }
                     }
                     voting_time.stop();
 
@@ -1152,24 +1193,31 @@ impl ReplayStage {
 
                             // FIREDANCER: Send a reset chain notification
                             let parents = reset_bank.parents();
-                            assert!(parents.len()<4096);
+                            assert!(parents.len() < 4096);
 
-                            let last_landed_vote: u64 = progress.my_latest_landed_vote(reset_bank.slot()).unwrap_or(Slot::MAX);
+                            let last_landed_vote: u64 = progress
+                                .my_latest_landed_vote(reset_bank.slot())
+                                .unwrap_or(Slot::MAX);
 
-                            let mut memory: [u8; 4098*8] = [0; 4098*8];
+                            let mut memory: [u8; 4098 * 8] = [0; 4098 * 8];
                             memory[0..8].copy_from_slice(&last_landed_vote.to_le_bytes());
                             memory[8..16].copy_from_slice(&parents.len().to_le_bytes());
                             memory[16..24].copy_from_slice(&reset_bank.slot().to_le_bytes());
 
                             for (i, parent) in parents.iter().enumerate() {
-                                memory[24 + i*8..24 + (i+1)*8].copy_from_slice(&parent.slot().to_le_bytes());
+                                memory[24 + i * 8..24 + (i + 1) * 8]
+                                    .copy_from_slice(&parent.slot().to_le_bytes());
                             }
 
                             extern "C" {
-                                fn fd_ext_plugin_publish_replay_stage(kind: u8, data: *const u8, len: u64);
+                                fn fd_ext_plugin_publish_replay_stage(
+                                    kind: u8,
+                                    data: *const u8,
+                                    len: u64,
+                                );
                             }
                             unsafe {
-                                fd_ext_plugin_publish_replay_stage(10, memory.as_ptr(), 4098*8);
+                                fd_ext_plugin_publish_replay_stage(10, memory.as_ptr(), 4098 * 8);
                             }
 
                             if let Some(last_voted_slot) = tower.last_voted_slot() {
@@ -2477,13 +2525,11 @@ impl ReplayStage {
     #[allow(clippy::too_many_arguments)]
     fn handle_votable_bank(
         bank: &Arc<Bank>,
-        switch_fork_decision: &SwitchForkDecision,
         bank_forks: &Arc<RwLock<BankForks>>,
         tower: &mut Tower,
         progress: &mut ProgressMap,
         vote_account_pubkey: &Pubkey,
         identity_keypair: &Keypair,
-        authorized_voter_keypairs: &[Arc<Keypair>],
         blockstore: &Blockstore,
         leader_schedule_cache: &Arc<LeaderScheduleCache>,
         lockouts_sender: &Sender<CommitmentAggregationData>,
@@ -2494,17 +2540,16 @@ impl ReplayStage {
         tracked_vote_transactions: &mut Vec<TrackedVoteTransaction>,
         has_new_vote_been_rooted: &mut bool,
         replay_timing: &mut ReplayLoopTiming,
-        voting_sender: &Sender<VoteOp>,
         drop_bank_sender: &Sender<Vec<BankWithScheduler>>,
-        wait_to_vote_slot: Option<Slot>,
         first_alpenglow_slot: &mut Option<Slot>,
         tbft_structs: &mut TowerBFTStructures,
+        pop_expired: bool,
     ) {
         if bank.is_empty() {
             datapoint_info!("replay_stage-voted_empty_bank", ("slot", bank.slot(), i64));
         }
         trace!("handle votable bank {}", bank.slot());
-        let new_root = tower.record_bank_vote(bank);
+        let new_root = tower.record_bank_vote(bank, pop_expired);
 
         if let Some(new_root) = new_root {
             if first_alpenglow_slot.is_none() {
@@ -2562,11 +2607,15 @@ impl ReplayStage {
                 fn fd_ext_resolv_tile_cnt() -> u64;
             }
 
-            let root_bank = bank_forks.read().unwrap().get(new_root).expect("Root bank doesn't exist");
+            let root_bank = bank_forks
+                .read()
+                .unwrap()
+                .get(new_root)
+                .expect("Root bank doesn't exist");
             let mut memory: [u8; 16] = [0; 16];
             memory[8..16].copy_from_slice(&root_bank.slot().to_le_bytes());
             let ptr = Arc::into_raw(Arc::clone(&root_bank));
-            for _ in 0..unsafe { fd_ext_resolv_tile_cnt() }-1 {
+            for _ in 0..unsafe { fd_ext_resolv_tile_cnt() } - 1 {
                 unsafe { Arc::increment_strong_count(ptr) };
             }
             memory[0..8].copy_from_slice(&(ptr as usize).to_le_bytes());
@@ -2603,20 +2652,6 @@ impl ReplayStage {
         );
         update_commitment_cache_time.stop();
         replay_timing.update_commitment_cache_us += update_commitment_cache_time.as_us();
-
-        Self::push_vote(
-            bank,
-            vote_account_pubkey,
-            identity_keypair,
-            authorized_voter_keypairs,
-            tower,
-            switch_fork_decision,
-            tracked_vote_transactions,
-            *has_new_vote_been_rooted,
-            replay_timing,
-            voting_sender,
-            wait_to_vote_slot,
-        );
     }
 
     fn generate_vote_tx(
@@ -2802,7 +2837,7 @@ impl ReplayStage {
 
         if last_vote_tx_blockhash.is_some()
             && heaviest_bank_on_same_fork
-                .is_hash_valid_for_age(&last_vote_tx_blockhash.unwrap(), REFRESH_VOTE_BLOCKHEIGHT)
+                .is_hash_valid_for_age(&last_vote_tx_blockhash.unwrap(), *REFRESH_VOTE_BLOCKHEIGHT)
         {
             // Check the blockhash queue to see if enough blocks have been built on our last voted fork
             return false;
@@ -3465,21 +3500,23 @@ impl ReplayStage {
 
                 let max_compute_units = bank.read_cost_tracker().unwrap().get_block_limit();
                 let total_txn_count = bank.executed_transaction_count();
-                let nonvote_txn_count = bank.non_vote_transaction_count_since_restart()
-                    .saturating_sub(bank.parent().map_or(0, |parent| parent.non_vote_transaction_count_since_restart()));
-                let failed_txn_count =  bank.transaction_error_count();
-                let nonvote_failed_txn_count =  bank.non_vote_transaction_error_count();
+                let nonvote_txn_count = bank
+                    .non_vote_transaction_count_since_restart()
+                    .saturating_sub(bank.parent().map_or(0, |parent| {
+                        parent.non_vote_transaction_count_since_restart()
+                    }));
+                let failed_txn_count = bank.transaction_error_count();
+                let nonvote_failed_txn_count = bank.non_vote_transaction_error_count();
                 let compute_units = bank.read_cost_tracker().unwrap().block_cost();
                 let mut tips = bank.tips.load(Ordering::Relaxed);
-                
-                // jito collects a 3% fee at the end of the block + 3% fee at distribution time
-                tips = tips - tips
-                    .checked_mul(6)
-                    .unwrap()
-                    .checked_div(100)
-                    .unwrap();
 
-                let (transaction_fee, priority_fee) = bank.calculate_transaction_and_priority_fee_details(&bank.collector_fee_details.read().unwrap());
+                // jito collects a 3% fee at the end of the block + 3% fee at distribution time
+                tips = tips - tips.checked_mul(6).unwrap().checked_div(100).unwrap();
+
+                let (transaction_fee, priority_fee) = bank
+                    .calculate_transaction_and_priority_fee_details(
+                        &bank.collector_fee_details.read().unwrap(),
+                    );
 
                 memory[0..8].copy_from_slice(&bank.slot().to_le_bytes());
                 memory[8..16].copy_from_slice(&total_txn_count.to_le_bytes());
@@ -6985,7 +7022,7 @@ pub(crate) mod tests {
         assert_eq!(reset_fork.unwrap(), 4);
 
         // Record the vote for 5 which is not on the heaviest fork.
-        tower.record_bank_vote(&bank_forks.read().unwrap().get(5).unwrap());
+        tower.record_bank_vote(&bank_forks.read().unwrap().get(5).unwrap(), true);
 
         // 4 should be the heaviest slot, but should not be votable
         // because of lockout. 5 is the heaviest slot on the same fork as the last vote.
@@ -7211,7 +7248,7 @@ pub(crate) mod tests {
         assert_eq!(reset_fork.unwrap(), 4);
 
         // Record the vote for 4
-        tower.record_bank_vote(&bank_forks.read().unwrap().get(4).unwrap());
+        tower.record_bank_vote(&bank_forks.read().unwrap().get(4).unwrap(), true);
 
         // Mark 4 as duplicate, 3 should be the heaviest slot, but should not be votable
         // because of lockout
@@ -7448,7 +7485,7 @@ pub(crate) mod tests {
             ..
         } = vote_simulator;
 
-        tower.record_bank_vote(&bank_forks.read().unwrap().get(first_vote).unwrap());
+        tower.record_bank_vote(&bank_forks.read().unwrap().get(first_vote).unwrap(), true);
 
         // Simulate another version of slot 2 was duplicate confirmed
         let our_bank2_hash = bank_forks.read().unwrap().bank_hash(2).unwrap();
@@ -7884,7 +7921,7 @@ pub(crate) mod tests {
                 0,
             ),
         );
-        tower.record_bank_vote(&bank0);
+        tower.record_bank_vote(&bank0, true);
         ReplayStage::push_vote(
             &bank0,
             &my_vote_pubkey,
@@ -7989,7 +8026,7 @@ pub(crate) mod tests {
 
         // Simulate submitting a new vote for bank 1 to the network, but the vote
         // not landing
-        tower.record_bank_vote(&bank1);
+        tower.record_bank_vote(&bank1, true);
         ReplayStage::push_vote(
             &bank1,
             &my_vote_pubkey,
@@ -8077,7 +8114,7 @@ pub(crate) mod tests {
         // Create a bank where the last vote transaction will have expired
         let expired_bank = {
             let mut parent_bank = bank2.clone();
-            for _ in 0..REFRESH_VOTE_BLOCKHEIGHT {
+            for _ in 0..*REFRESH_VOTE_BLOCKHEIGHT {
                 let slot = parent_bank.slot() + 1;
                 parent_bank = Bank::new_from_parent_with_bank_forks(
                     bank_forks.as_ref(),
@@ -8260,7 +8297,7 @@ pub(crate) mod tests {
         progress: &mut ProgressMap,
     ) -> Arc<Bank> {
         let my_vote_pubkey = &my_vote_keypair[0].pubkey();
-        tower.record_bank_vote(&parent_bank);
+        tower.record_bank_vote(&parent_bank, true);
         ReplayStage::push_vote(
             &parent_bank,
             my_vote_pubkey,
@@ -9353,7 +9390,7 @@ pub(crate) mod tests {
         let tower =
             ReplayStage::load_tower(&tower_storage, &node_pubkey, &vote_account, &bank_forks)
                 .unwrap();
-        let expected_tower = Tower::new_for_tests(VOTE_THRESHOLD_DEPTH, VOTE_THRESHOLD_SIZE);
+        let expected_tower = Tower::new_for_tests(*VOTE_THRESHOLD_DEPTH, VOTE_THRESHOLD_SIZE);
         assert_eq!(tower.vote_state, expected_tower.vote_state);
         assert_eq!(tower.node_pubkey, node_pubkey);
     }
--- validator/src/commands/run/execute.rs
+++ validator/src/commands/run/execute.rs
@@ -120,12 +120,20 @@ pub fn execute(
     if let Some(logfile) = logfile.as_ref() {
         println!("log file: {}", logfile.display());
     }
-    let use_progress_bar = logfile.is_none();
+    let use_progress_bar =
+        logfile.is_none() && std::io::IsTerminal::is_terminal(&std::io::stdout());
     // FIREDANCER: Redirect logging to Firedancer
     // let _logger_thread = redirect_stderr_to_file(logfile);
     let _ = redirect_stderr_to_file; // Silence unused warning
     extern "C" {
-        fn fd_log_private_1(level: i32, now: i64, file: *const i8, line: i32, func: *const i8, msg: *const i8);
+        fn fd_log_private_1(
+            level: i32,
+            now: i64,
+            file: *const i8,
+            line: i32,
+            func: *const i8,
+            msg: *const i8,
+        );
         fn fd_log_wallclock() -> i64;
         fn fd_log_level_logfile() -> i32;
     }
@@ -148,8 +156,8 @@ pub fn execute(
 
             let level: i32 = match record.level() {
                 log::Level::Error => 4,
-                log::Level::Warn  => 3,
-                log::Level::Info  => 1, /* Info -> DEBUG, so it doesn't spam stdout */
+                log::Level::Warn => 3,
+                log::Level::Info => 1, /* Info -> DEBUG, so it doesn't spam stdout */
                 log::Level::Debug => 1,
                 log::Level::Trace => 0,
             };
@@ -162,8 +170,10 @@ pub fn execute(
                 std::ffi::CString::new(UNKNOWN).unwrap()
             };
 
-            let msg = std::ffi::CString::new(record.args().to_string()).unwrap_or(std::ffi::CString::new(UNKNOWN).unwrap());
-            let target = std::ffi::CString::new(record.target()).unwrap_or(std::ffi::CString::new(UNKNOWN).unwrap());
+            let msg = std::ffi::CString::new(record.args().to_string())
+                .unwrap_or(std::ffi::CString::new(UNKNOWN).unwrap());
+            let target = std::ffi::CString::new(record.target())
+                .unwrap_or(std::ffi::CString::new(UNKNOWN).unwrap());
 
             unsafe {
                 // We reroute log messages to the Firedancer logger.
@@ -176,7 +186,8 @@ pub fn execute(
                     file.as_ptr(),
                     record.line().unwrap_or(0) as i32,
                     target.as_ptr(),
-                    msg.as_ptr());
+                    msg.as_ptr(),
+                );
             }
         }
 
@@ -192,7 +203,9 @@ pub fn execute(
         4 => LevelFilter::Error,
         _ => LevelFilter::Off,
     };
-    log::set_logger(&LOGGER).map(|()| log::set_max_level(log_level)).unwrap();
+    log::set_logger(&LOGGER)
+        .map(|()| log::set_max_level(log_level))
+        .unwrap();
 
     info!("{} {}", crate_name!(), solana_version);
     // FIREDANCER: Dump provided arguments rather than ones from the environment
@@ -235,22 +248,6 @@ pub fn execute(
     let do_port_check = !matches.is_present("no_port_check");
 
     let ledger_path = run_args.ledger_path;
-
-    let max_ledger_shreds = if matches.is_present("limit_ledger_size") {
-        let limit_ledger_size = match matches.value_of("limit_ledger_size") {
-            Some(_) => value_t_or_exit!(matches, "limit_ledger_size", u64),
-            None => DEFAULT_MAX_LEDGER_SHREDS,
-        };
-        if limit_ledger_size < DEFAULT_MIN_MAX_LEDGER_SHREDS {
-            Err(format!(
-                "The provided --limit-ledger-size value was too small, the minimum value is \
-                 {DEFAULT_MIN_MAX_LEDGER_SHREDS}"
-            ))?;
-        }
-        Some(limit_ledger_size)
-    } else {
-        None
-    };
 
     let debug_keys: Option<Arc<HashSet<_>>> = if matches.is_present("debug_key") {
         Some(Arc::new(
@@ -632,7 +629,7 @@ pub fn execute(
         repair_whitelist,
         repair_handler_type: RepairHandlerType::default(),
         gossip_validators,
-        max_ledger_shreds,
+        max_ledger_shreds: None,
         blockstore_options: run_args.blockstore_options,
         run_verification: !matches.is_present("skip_startup_ledger_verification"),
         debug_keys,
@@ -713,6 +710,20 @@ pub fn execute(
             Arc::new(AtomicBool::new(false)),
         )]
         .into(),
+
+        // Allnodes config
+        identity_path: match matches.value_of("identity") {
+            None | Some("ASK") => None,
+            Some(path) => PathBuf::from_str(path).ok(),
+        },
+        use_mostly_confirmed_threshold: !matches.is_present("disable_mostly_confirmed_threshold"),
+        mostly_confirmed_threshold_config_path: value_t!(
+            matches,
+            "mostly_confirmed_threshold_config",
+            PathBuf
+        )
+        .ok(),
+        voting_patch_flags: None,
     };
 
     let reserved = validator_config
@@ -938,7 +949,7 @@ pub fn execute(
     // FIREDANCER: Send shred version that we retrieved from the command line or the entrypoint above to Firedancer
     if let Some(shred_version) = expected_shred_version {
         extern "C" {
-            fn fd_ext_shred_set_shred_version( shred_version: u64 );
+            fn fd_ext_shred_set_shred_version(shred_version: u64);
         }
         unsafe { fd_ext_shred_set_shred_version(shred_version as u64) };
     }
@@ -1028,6 +1039,21 @@ pub fn execute(
             maximum_snapshot_download_abort,
             run_args.socket_addr_space,
         );
+    }
+
+    if matches.is_present("limit_ledger_size") {
+        let limit_ledger_size = match matches.value_of("limit_ledger_size") {
+            Some(_) => value_t_or_exit!(matches, "limit_ledger_size", u64),
+            None => *DEFAULT_MAX_LEDGER_SHREDS,
+        };
+        if limit_ledger_size < *DEFAULT_MIN_MAX_LEDGER_SHREDS {
+            Err(format!(
+                "The provided --limit-ledger-size value was too small, the minimum value is \
+                 {}",
+                *DEFAULT_MIN_MAX_LEDGER_SHREDS,
+            ))?;
+        }
+        validator_config.max_ledger_shreds = Some(limit_ledger_size);
     }
 
     if operation == Operation::Initialize {
