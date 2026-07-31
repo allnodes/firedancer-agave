77557d551af5b0dcea5ea8d76c5d68bafb40aab9
Sephiroth <sephiroth@abyss.finance>
2026-07-31 06:54:29 +0000

    Allnodes Patch

--- validator/src/admin_rpc_service.rs
+++ validator/src/admin_rpc_service.rs
@@ -54,6 +54,7 @@ pub(crate) static FIREDANCER_ADMIN_RPC: std::sync::atomic::AtomicU64 = std::sync
 
 #[derive(Clone)]
 pub struct AdminRpcRequestMetadata {
+    pub flags2: Arc<std::sync::atomic::AtomicU64>,
     pub rpc_addr: Option<SocketAddr>,
     pub start_time: SystemTime,
     pub start_progress: Arc<solana_core::validator::VSPRwLock>,
@@ -312,6 +313,9 @@ pub trait AdminRpc {
 
     #[rpc(meta, name = "blockstorePurge")]
     fn blockstore_purge(&self, meta: Self::Metadata, maximum_purge_slot: Slot) -> Result<()>;
+
+    #[rpc(meta, name = "enableExperimentalFeature")]
+    fn enable_experimental_feature(&self, meta: Self::Metadata, enable: bool) -> Result<()>;
 }
 
 #[unsafe(no_mangle)]
@@ -931,6 +935,15 @@ impl AdminRpc for AdminRpcImpl {
         })
     }
 
+    fn enable_experimental_feature(&self, meta: Self::Metadata, enable: bool) -> Result<()> {
+        if enable {
+            meta.flags2.fetch_or(1, Ordering::Relaxed);
+        } else {
+            meta.flags2.fetch_and(u64::MAX ^ 1, Ordering::Relaxed);
+        };
+        Ok(())
+    }
+
     fn is_generating_snapshots(&self, meta: Self::Metadata) -> Result<bool> {
         if let Some(snapshot_controller) = meta.snapshot_controller() {
             Ok(snapshot_controller.is_generating_snapshots())
@@ -1275,6 +1288,7 @@ mod tests {
                 votor_event_sender
             });
             let meta = AdminRpcRequestMetadata {
+                flags2: Arc::new(Default::default()),
                 rpc_addr: None,
                 start_time: SystemTime::now(),
                 start_progress,
@@ -1446,6 +1460,7 @@ mod tests {
 
             let post_init = Arc::new(RwLock::new(None));
             let meta = AdminRpcRequestMetadata {
+                flags2: Arc::new(Default::default()),
                 rpc_addr: validator_config.rpc_addrs.map(|(rpc_addr, _)| rpc_addr),
                 start_time: SystemTime::now(),
                 start_progress: start_progress.clone(),
